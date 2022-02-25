c061a89ac5cd155bcadc3c733643cdaa7ac53a05
Qrexx <59852162+QrexxWasTaken@users.noreply.github.com>
2022-02-25 19:08:06 +0000

    added sounds and randomly spawning beans

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var game = {
     beans: 0,
+    clicks: 0,
+    lifetimeBeans: 0,
     faction: null,
+    clickpower: 1,
     upgrades: {
         beanMachine: {
             amount: 0,
@@ -59,24 +62,80 @@ var game = {
             name: "Bean Goverment"
         }
     },
-    acheives: [{ req: "game.beans>0", gotten: false, text: "You have one BEAN" }, { req: "game.beans>9", gotten: false, text: "You have ten BEANS" }, { req: "game.beans>68", gotten: false, text: "You have some nice BEANS" }, { req: "game.beans>9999999999", gotten: false, text: "1%- of what?" }, { req: "game.beans>49999999999", gotten: false, text: "5% of something" }, { req: "game.beans>99999999999", gotten: false, text: "10%..." }, { req: "game.beans>499999999999", gotten: false, text: "50%, something's talking to me..." }, { req: "game.beans>999999999999", gotten: false, text: "The BEANPOCALYPSE - coming soon lol" }]
+    acheives: [{ req: "game.beans>0", gotten: false, text: "You have one BEAN" }, { req: "game.beans>9", gotten: false, text: "You have ten BEANS" }, { req: "game.beans>68", gotten: false, text: "69 beans- nice" }, { req: "game.beans>999999&&game.clicks<=10", gotten: false, text: "Neverclick- get 1,000,000 beans<br>and only click 10 times" }, { req: "game.beans>999999&&game.clicks==0", gotten: false, text: "True Neverclick- get 1,000,000<br>beans without clicking" }, { req: "game.beans>9999999999", gotten: false, text: "1%- of what?" }, { req: "game.beans>49999999999", gotten: false, text: "5% of something" }, { req: "game.beans>99999999999", gotten: false, text: "10%..." }, { req: "game.beans>499999999999", gotten: false, text: "50%, something's talking to me..." }, { req: "game.beans>999999999999", gotten: false, text: "The BEANPOCALYPSE - coming soon lol" }]
 };
 
 var delay = 0;
 var bps = 0;
+var beanclick = document.querySelector("#beanclick");
+var click = document.querySelector("#clicksfx");
+
+function beaner() {
+    document.querySelector(".BEAN").src = "/images/beaner.png";
+}
 
 function updateUpgrades() {
     var _d = 0;
     document.querySelector("#upgrades").innerHTML = "";
     for (_ in game.upgrades) {
         if (game.upgrades[_].hasun) {
-            document.querySelector("#upgrades").innerHTML += `<br> <button class="buy-btn" onclick="thingClicked('${_}')">${game.upgrades[_].name}</button> you have ${numberformat.format(game.upgrades[_].amount)}. Cost: ${numberformat.format(game.upgrades[_].cost)}`;
+            document.querySelector("#upgrades").innerHTML += `<br> <button class="buy-btn" onclick="thingClicked('${_}')"><span style="float:left;">${game.upgrades[_].name}<br>${numberformat.format(game.upgrades[_].cost)} beans</span><span style="float:right;">${numberformat.format(game.upgrades[_].amount)}</span></button>`;
             _d += game.upgrades[_].bps * game.upgrades[_].amount;
             bps = _d;
         }
     }
 }
 
+function beanClick(){
+    if(document.querySelector("#popupbean").style.display != "none"){
+        var choice = Math.ceil(Math.random()*10)/2;
+        console.log(choice);
+        document.querySelector("#popupbean").style.display = "none";
+        beanclick.play();
+        if(choice <= 1){
+            game.beans += Math.round(Math.random()*100);
+        }else if(choice <= 2 && choice > 1){
+            var oldcp = game.clickpower;
+            game.clickpower *= 7;
+            setTimeout(() => {
+                game.clickpower = oldcp;
+            },30000)
+        }else if(choice <= 3 && choice > 2){
+            console.log("2.1 -> 3")
+        }else if(choice <= 4 && choice > 3){
+            console.log("3.1 -> 4");
+        }else if(choice <= 5 && choice > 4){
+            console.log("4.1 -> 5");
+        }
+    }else{
+        return false;
+    }
+}
+
+function summonBean() {
+    var bean = document.querySelector("#popupbean");
+    var height = window.innerHeight - bean.height;
+    var width = window.innerWidth - bean.width;
+    var x = Math.round(Math.random()*width);
+    var y = Math.round(Math.random()*height);
+    if(x < 0){
+        x *= -1
+    }
+    if(y < 0){
+        y *= -1
+    }
+    console.log(x,y)
+    bean.style.top = `${y}px`;
+    bean.style.left = `${x}px`;
+    bean.style.display = "block";
+    bean.addEventListener('click', beanClick);
+    bean.style.transform = `rotate(${Math.round(Math.random()*1000)}deg)`;
+    setTimeout(() => {
+        bean.style.display = "none";
+        setTimeout(summonBean,5000)
+    },5000)
+}
+            
 function updateCount() {
     if (Cookies.get("game") != null && Cookies.get("game") != "undefined") {
         var game1 = JSON.parse(Cookies.get("game"));
@@ -111,7 +170,7 @@ function updateCount() {
             var _b = new Function('return ' + game.acheives[_].req);
             if (_b() & !game.acheives[_].gotten) {
                 game.acheives[_].gotten = true;
-                document.querySelector("#acheives").innerHTML += `<br>Acheivement Unlocked<br>${game.acheives[_].text}<br>`;
+                document.querySelector("#acheives").innerHTML += `<br><div class='acheivement'>Acheivement Unlocked<br>${game.acheives[_].text}</div><br>`;
             }
         }
         document.querySelector("#beans").innerHTML = `You have ${numberformat.format(Number(String(game.beans).split(".")[0]))} Beans`;
@@ -129,6 +188,9 @@ function updateCount() {
         }
 
     }, 50)
+    setTimeout(() => {
+        summonBean();
+    },5000)
 }
 
 function thingClicked(thing) {
@@ -138,6 +200,10 @@ function thingClicked(thing) {
         game.upgrades[thing].cost += Math.round(game.upgrades[thing].cost * 0.15)
         updateUpgrades();
     }
+    click.play();
 }
 
 console.log("%cAre you trying to CHEAT?!", "color:red;font-size:36px;");
+Notification.requestPermission().then(function(result) {
+  console.log(`The user ${result} notifications`);
+});
