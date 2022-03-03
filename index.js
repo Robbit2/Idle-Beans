f4a28f25062af164ccc34df4dd7bf9b7f9c45e86
Qrexx <59852162+QrexxWasTaken@users.noreply.github.com>
2022-03-03 01:06:19 +0000

    Version 0.1.2 - added wrath beans and reset function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*
+All code copyright Joseph Heinz, 2022
+    (with help from stackoverflow and cukmekerb (definitively not stolen with credit))
+*/
+
 var game = {
     beans: 0,
     clicks: 0,
@@ -5,64 +10,168 @@ var game = {
     faction: null,
     clickpower: 1,
     upgrades: {
-        beanMachine: {
+        beanCan: {
             amount: 0,
             cost: 10,
             bps: 0.1,
             hasun: false,
             unlocked: 1,
-            name: "Bean Machine"
+            name: "Can o' beans",
+            flavor: "A can to keep your beans in- YUM!",
+            img: "/images/bean-can.png"
         },
-        beanField: {
+        beanMachine: {
             amount: 0,
             cost: 100,
             bps: 1,
             hasun: false,
             unlocked: 75,
-            name: "Bean Field"
+            name: "Machine",
+            flavor: "A machine that packages up beans",
+            img: "/images/buildings/bean-machine.png"
         },
-        beanAcademy: {
+        beanFarm: {
             amount: 0,
             cost: 1500,
-            bps: 5,
+            bps: 10,
             hasun: false,
             unlocked: 1250,
-            name: "Bean Academy"
+            name: "Farm",
+            flavor: "Plant beans, to grow, MORE BEANS",
+            img: "/images/buildings/bean-farm.png"
+        },
+        beanDrill: {
+            amount: 0,
+            cost: 170000,
+            bps: 200,
+            hasun: false,
+            unlocked: 150000,
+            name: "Drill",
+            flavor: "Drills into the ground to find deposits of beans",
+            img: "/images/buildings/bean-drill.png"
         },
         beanFactory: {
             amount: 0,
-            cost: 170000,
-            bps: 30,
-            hasun: false,
-            unlocked: 150000,
-            name: "Bean Factory"
-        },
-        beanReserve: {
-            amount: 0,
             cost: 5000000,
-            bps: 160,
+            bps: 3600,
             hasun: false,
             unlocked: 4500000,
-            name: "Bean Reserve"
+            name: "Factory",
+            flavor: "Packages beans to send all around the world",
+            img: "/images/buildings/bean-factory.png"
+        },
+        beanTreasury: {
+            amount: 0,
+            cost: 300000000,
+            bps: 54000,
+            hasun: false,
+            unlocked: 25000000,
+            name: "Treasury",
+            flavor: "Keeps beans safe and accepts<br>them as legal tender",
+            img: "/images/buildings/bean-treasury.png"
         },
         beanTemple: {
             amount: 0,
-            cost: 300000000,
-            bps: 2400,
-            hasun: false,
-            unlocked: 25000000,
-            name: "Bean Temple"
-        },
-        beanGovernment: {
-            amount: 0,
             cost: 1400000000,
-            bps: 13000,
+            bps: 130000,
             hasun: false,
             unlocked: 1300000000,
-            name: "Bean Goverment"
+            name: "Temple",
+            flavor: "A place to pray to the great bean can in the sky",
+            img: "/images/buildings/bean-temple.png"
+        },
+        beanLaboratory: {
+            amount: 0,
+            cost: 33000000000,
+            bps: 1250000,
+            hasun: false,
+            unlocked: 25000000000,
+            name: "Laboratory",
+            flavor: "Synthesising beans is a great step for humanity",
+            img: "/images/buildings/bean-lab.png"
+        },
+        beanRocket: {
+            amount: 0,
+            cost: 420000000000,
+            bps: 16000000,
+            hasun: false,
+            unlocked: 400000000000,
+            name: "Rocket",
+            flavor: "The final frontier doesn't seem so final now",
+            img: "/images/buildings/bean-rocket.png"
+        },
+        beanMagician: {
+            amount: 0,
+            cost: 1000000000000,
+            bps: 52000000,
+            hasun: false,
+            unlocked: 900000000000,
+            name: "Magician",
+            flavor: "Creating beans with MAGIC",
+            img: "/images/buildings/magician.png"
+        },
+        quantumBeans: {
+            amount: 0,
+            cost: 16500000000000,
+            bps: 244000000,
+            hasun: false,
+            unlocked: 1500000000000,
+            name: "Quantum Beans",
+            flavor: "How can a bean be a bean and a lentil? Oh wait, nevermind",
+            img: "/images/buildings/quantum-bean.png"
+        },
+        darkMatterEnhancer: {
+            amount: 0,
+            cost: 670500000000000,
+            bps: 1200000000,
+            hasun: false,
+            unlocked: 600000000000000,
+            name: "Dark Matter Enhancer",
+            flavor: "'Creating beans from invisible energy since 1997™'",
+            img: "/images/buildings/dark-matter-enhancer.png"
+        },
+        lightRefractor: {
+            amount: 0,
+            cost: 1300000000000000,
+            bps: 16000000000,
+            hasun: false,
+            unlocked: 1000000000000000,
+            name: "Light Refractor",
+            flavor: "Creating beans from pure light",
+            img: "/images/buildings/light-refractor.png"
+        },
+        beanRecycler: {
+            amount: 0,
+            cost: 14000000000000000,
+            bps: 475000000000,
+            hasun: false,
+            unlocked: 10000000000000000,
+            name: "Recycler",
+            flavor: "Recycles organic and non-organic matter into beans",
+            img: "/images/buildings/recycler.png"
+        },
+        gameDeveloper: {
+            amount: 0,
+            cost: 420690000000000000,
+            bps: 6900000000000,
+            hasun: false,
+            unlocked: 420000000000000000,
+            name: "Game Dev",
+            flavor: "Creates games to make more beans",
+            img: "/images/buildings/game-dev.png"
+        },
+        mulitversalCore: {
+            amount: 0,
+            cost: 1600000000000000000,
+            bps: 23000000000000,
+            hasun: false,
+            unlocked: 1000000000000000000,
+            name: "Multiversal Core",
+            flavor: "Takes beans from other multiverses",
+            img: "/images/buildings/multiversal-core.png"
         }
     },
-    acheives: [{ req: "game.beans>0", gotten: false, text: "You have one BEAN" }, { req: "game.beans>9", gotten: false, text: "You have ten BEANS" }, { req: "game.beans>68", gotten: false, text: "69 beans- nice" }, { req: "game.beans>999999&&game.clicks<=10", gotten: false, text: "Neverclick- get 1,000,000 beans<br>and only click 10 times" }, { req: "game.beans>999999&&game.clicks==0", gotten: false, text: "True Neverclick- get 1,000,000<br>beans without clicking" }, { req: "game.beans>9999999999", gotten: false, text: "1%- of what?" }, { req: "game.beans>49999999999", gotten: false, text: "5% of something" }, { req: "game.beans>99999999999", gotten: false, text: "10%..." }, { req: "game.beans>499999999999", gotten: false, text: "50%, something's talking to me..." }, { req: "game.beans>999999999999", gotten: false, text: "The BEANPOCALYPSE - coming soon lol" }]
+    acheives: [{ req: "game.beans>0", gotten: false, text: "You have one BEAN", img: "/images/acheivements/bean-acheive-64x.png" }, { req: "game.beans>9", gotten: false, text: "You have ten BEANS", img: "/images/acheivements/10-beans-acheive-64x.png" }, { req: "game.beans>68", gotten: false, text: "69 beans- nice", img: "/images/acheivements/681-beans-acheive-64x.png" }, { req: "game.beans>999999&&game.clicks<=10", gotten: false, text: "Neverclick- get 1,000,000 beans<br>and only click 10 times", img: "/images/acheivements/neverclick-acheive-64x.png" }, { req: "game.beans>999999&&game.clicks==0", gotten: false, text: "True Neverclick- get 1,000,000<br>beans without clicking", img: "/images/acheivements/true-neverclick-acheive-64x.png" }]
 };
 
 var delay = 0;
@@ -79,17 +188,44 @@ sfx_error.defaultPlaybackRate = 0.5;
 var sfx_acheiveGet = new Audio("/audio/acheiveGet.wav");
 sfx_acheiveGet.volume = .5;
 sfx_acheiveGet.defaultPlaybackRate = 0.5;
+var sfx_wrathbean = new Audio("/audio/wrathbean.wav");
+sfx_wrathbean.volume = .5;
+sfx_wrathbean.defaultPlaybackRate = 0.5;
 
 function beaner() {
     document.querySelector(".BEAN").src = "/images/beaner.png";
 }
 
+function reset(){
+    if(confirm("Are you sure you want to reset?\n!!!THIS IS NOT ASCENDING MOSTLY BECAUSE THAT ISN'T A FEATURE!!!")){
+        game.beans = 0;
+        game.clicks = 0;
+        game.faction = null;
+        game.clickpower = 1;
+        game.lifetimeBeans = 0;
+        var _prices = [10,100,1500,170000,5000000,300000000,1400000000,33000000000,420000000000,1000000000000,16500000000000,670500000000000,1300000000000000,14000000000000000,420690000000000000,1600000000000000000];
+        var _iter = 0;
+        for(_ in game.upgrades){
+            game.upgrades[_].amount = 0;
+            game.upgrades[_].hasun = false;
+            game.upgrades[_].cost = _prices[_iter];
+            console.log(_);
+            _iter ++;
+        }
+        
+        for(_ in game.acheives){
+            game.acheives[_].gotten = false;
+        }
+        Cookies.remove("game");
+    }
+}
+
 function updateUpgrades() {
     var _d = 0;
-    document.querySelector("#upgrades").innerHTML = "";
+    document.querySelector("#buildings").innerHTML = "";
     for (_ in game.upgrades) {
         if (game.upgrades[_].hasun) {
-            document.querySelector("#upgrades").innerHTML += `<br> <button class="buy-btn" onclick="thingClicked('${_}')"><span style="float:left;">${game.upgrades[_].name}<br>${numberformat.format(game.upgrades[_].cost)} beans</span><span style="float:right;">${numberformat.format(game.upgrades[_].amount)}</span></button>`;
+            document.querySelector("#buildings").innerHTML += `<br> <button class="buy-btn" onclick="thingClicked('${_}')"><img src="${game.upgrades[_].img}" alt="${game.upgrades[_].img}" height="64px" width="64px" style="float:left;margin:2px;"><span style="text-align:left;float:left;font-size:16px;">${game.upgrades[_].name}<br>${numberformat.format(game.upgrades[_].cost)} beans<br>${game.upgrades[_].flavor}</span><span style="float:right;font-size:32px;">${numberformat.format(game.upgrades[_].amount)}</span></button>`;
             _d += game.upgrades[_].bps * game.upgrades[_].amount;
             bps = _d;
         }
@@ -103,7 +239,13 @@ function beanClick(){
         document.querySelector("#popupbean").style.display = "none";
         sfx_beanClick.play();
         if(choice <= 1){
-            game.beans += Math.round(Math.random()*100);
+            if(game.beans == 0){
+                game.beans += Math.round(Math.random()*25);
+            }else if(game.beans >= 1 && game.beans <= 10){
+                game.beans += Math.round(Math.random()*100);
+            }else{
+                game.beans += Math.round(Math.random()*game.beans/1.5);
+            }
         }else if(choice <= 2 && choice > 1){
             var oldcp = game.clickpower;
             game.clickpower *= 7;
@@ -111,11 +253,27 @@ function beanClick(){
                 game.clickpower = oldcp;
             },30000)
         }else if(choice <= 3 && choice > 2){
-            console.log("2.1 -> 3")
+            if(game.beans == 0){
+                game.beans += Math.round(Math.random()*25);
+            }else if(game.beans >= 1 && game.beans <= 10){
+                game.beans += Math.round(Math.random()*100);
+            }else{
+                game.beans += Math.round(Math.random()*game.beans/1.5);
+            }
         }else if(choice <= 4 && choice > 3){
-            console.log("3.1 -> 4");
+            if(game.beans == 0){
+                game.beans += Math.round(Math.random()*25);
+            }else if(game.beans >= 1 && game.beans <= 10){
+                game.beans += Math.round(Math.random()*100);
+            }else{
+                game.beans += Math.round(Math.random()*game.beans/1.5);
+            }
         }else if(choice <= 5 && choice > 4){
-            console.log("4.1 -> 5");
+            var oldcp = game.clickpower;
+            game.clickpower *= 7;
+            setTimeout(() => {
+                game.clickpower = oldcp;
+            },30000)
         }
     }else{
         return false;
@@ -144,6 +302,43 @@ function summonBean() {
         bean.style.display = "none";
         setTimeout(summonBean,5000)
     },420000)
+}
+
+function wrathbeanClick(){
+    if(document.querySelector("#popupwrathbean").style.display != "none"){
+        var choice = Math.ceil(Math.random()*10)/2;
+        console.log(choice);
+        document.querySelector("#popupwrathbean").style.display = "none";
+        sfx_wrathbean.play();
+        game.beans -= 1;
+        alert("Lmao you clicked the wrath bean and lost 1 bean. Cope.");
+    }else{
+        return false;
+    }
+}
+
+function summonWrath() {
+    var bean = document.querySelector("#popupwrathbean");
+    var height = window.innerHeight - bean.height;
+    var width = window.innerWidth - bean.width;
+    var x = Math.round(Math.random()*width);
+    var y = Math.round(Math.random()*height);
+    if(x < 0){
+        x *= -1
+    }
+    if(y < 0){
+        y *= -1
+    }
+    console.log(x,y)
+    bean.style.top = `${y}px`;
+    bean.style.left = `${x}px`;
+    bean.style.display = "block";
+    bean.addEventListener('click', wrathbeanClick);
+    bean.style.transform = `rotate(${Math.round(Math.random()*1000)}deg)`;
+    setTimeout(() => {
+        bean.style.display = "none";
+        setTimeout(summonWrath,1800000)
+    },1800000)
 }
             
 function updateCount() {
@@ -180,7 +375,7 @@ function updateCount() {
             var _b = new Function('return ' + game.acheives[_].req);
             if (_b() & !game.acheives[_].gotten) {
                 game.acheives[_].gotten = true;
-                document.querySelector("#acheives").innerHTML += `<br><div class='acheivement'>Acheivement Unlocked<br>${game.acheives[_].text}</div><br>`;
+                document.querySelector("#acheives").innerHTML += `<br><div class='acheivement'><img src='${game.acheives[_].img}' style='height:64px;width:64px;float:left;' alt='acheiveIMG'>Acheivement Unlocked<br>${game.acheives[_].text}</div><br>`;
                 sfx_acheiveGet.play();
                 if (window.Notification && Notification.permission === "granted") {
                     var img = "/images/idle_beans_icon.png";
@@ -203,9 +398,20 @@ function updateCount() {
         }
 
     }, 50)
+    setInterval(() => {
+        var _bps = 0;
+        for(_ in game.upgrades){
+            _bps += game.upgrades[_].amount * game.upgrades[_].bps;
+        }
+        document.querySelector("#bps").innerHTML = `beans per second: ${numberformat.format(_bps)}`;
+        document.title = `${numberformat.format(game.beans)} beans // Idle Beans`;
+    },500)
     setTimeout(() => {
         summonBean();
     },420000)
+    setTimeout(() => {
+        summonWrath();
+    },1800000)
 }
 
 function thingClicked(thing) {
